bc414c152342d48730ff5a4cf2417b0f58ef364d
Adler180 <41586377+Adler180@users.noreply.github.com>
2022-04-27 10:24:08 +0200

    Created gcd.py

---
+++ gcd.py
@@ -0,0 +1,13 @@
+def gcd(r0, r1):
+	#Returns the greatest common divisor of r0 and r1
+    if r0 == 0:
+        return r1
+    else:
+        if r1 == 0:
+            return r0
+        else:
+            if (r0 % r1 == 0):
+                return r1
+            else:
+                temp = r0 %r1
+                return ggt(r1, temp)
